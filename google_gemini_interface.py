c763bc608ce271244aaebeacfd373a123649ae36
Eduardo Villalpando Mello <eduardo.villalpando.mello@gmail.com>
2024-04-13 18:22:05 -0600

    [ADD] google_gemini_interface: summarize tweets

--- google_gemini_interface.py
+++ google_gemini_interface.py
@@ -11,6 +11,35 @@ import os
 import google.generativeai as genai
 genai.configure(api_key=os.getenv('GOOGLE_GEMINI_API_KEY'))
 
-model = genai.GenerativeModel('gemini-pro')
-response = model.generate_content("What is the meaning of life?")
-print(response.text)
+def summarize_list_of_tweets(tweets):
+    model = genai.GenerativeModel('gemini-pro')
+    # See this for prompt generation: https://ai.google.dev/docs/concepts#generate-list
+    response = model.generate_content(f"""
+        Give me a one sentence summary of the following tweets from different authors: \n \n
+        {'\n'.join(tweets)}
+    """)
+    return response.text
+
+tweet_objects = [
+    {
+        "id": 1,
+        "text": "I love my dog",
+        "user": "user1",
+        "created_at": "2022-01-01"
+    },
+    {
+        "id": 2,
+        "text": "I love my cat",
+        "user": "user2",
+        "created_at": "2022-01-02"
+    },
+    {
+        "id": 3,
+        "text": "I love my fish",
+        "user": "user3",
+        "created_at": "2022-01-03"
+    },
+]
+ 
+summary = summarize_list_of_tweets([tweet['text'] for tweet in tweet_objects])
+print(summary)
