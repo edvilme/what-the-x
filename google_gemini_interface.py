4e6c763d652eb014d00c1e31fabfd033c3d4be80
komugi-al <alcl@odoo.com>
2024-04-20 10:07:24 -0600

    [ADD] Added new classes and test file
    Functions are now part of a class and a class format exists to be used within each function about a question type.
    A data file to test the code has been added as well

--- google_gemini_interface.py
+++ google_gemini_interface.py
@@ -6,66 +6,103 @@
 from dotenv import load_dotenv
 load_dotenv()
 
+import json
 import os
 
 import google.generativeai as genai
 genai.configure(api_key=os.getenv('GOOGLE_GEMINI_API_KEY'))
 
-def summarize_list_of_tweets(tweets):
-    model = genai.GenerativeModel('gemini-pro')
-    # See this for prompt generation: https://ai.google.dev/docs/concepts#generate-list
-    response = model.generate_content(f"""
-        Give me a one sentence summary of the following tweets from different authors: \n \n
-        {'\n'.join(tweets)}
-    """)
-    return response.text
+class JSONFormat:
+    BASE_FORMAT = {
+        "id": "1",
+        "topic": "The topic of the tweet",
+    }
 
-def generate_question_trivia(question):
-    model = genai.GenerativeModel('gemini-pro')
-    response = model.generate_content(f"""
-        Create a one paragraph summary of the following tweets from different authors to generate trivia questions.
-        Use the json format: {{"topic": "trivia", "question": "What is the question?", "options": ["option1", "option2", "option3", "option4"], "answer": "option1}}
-        Use the following tweets to generate the questions: \n \n
-        {'\n'.join(question)}
-    """)
-    return response.text
+    def _get_options_format(self):
+        return {
+            **self.BASE_FORMAT,
+            "question": "What is the question?",
+            "options": "The array of options for the question, containing the correct answer",
+            "answer": "The correct option from the options as a list of one string"
+        }
 
-def generate_question_guess_author(tweets):
-    model = genai.GenerativeModel('gemini-pro')
-    response = model.generate_content(f"""
-        Generate a question asking to guess the author of the following tweets: \n \n
-        {'\n'.join(tweets)}
-    """)
-    return response.text
+    def _get_complete_format(self):
+        return {
+            **self.BASE_FORMAT,
+            "question": "The tweet with a blank space to complete",
+            "options": "The array of options for the blank space, containing the correct answer",
+            "answer": "The correct option from the options as a list of one string"
+        }
+    
 
-def generate_question_complete_tweet(tweets):
-    model = genai.GenerativeModel('gemini-pro')
-    response = model.generate_content(f"""
-        Generate a question asking to complete the following tweets: \n \n
-        {'\n'.join(tweets)}
-    """)
-    return response.text
+class GeminiInterface:
+    def __init__(self):
+        self.model = genai.GenerativeModel('gemini-pro')
+        self.format = JSONFormat()
 
-tweet_objects = [
-    {
-        "id": 1,
-        "text": "I love my dog",
-        "user": "user1",
-        "created_at": "2022-01-01"
-    },
-    {
-        "id": 2,
-        "text": "I love my cat",
-        "user": "user2",
-        "created_at": "2022-01-02"
-    },
-    {
-        "id": 3,
-        "text": "I love my fish",
-        "user": "user3",
-        "created_at": "2022-01-03"
-    },
-]
- 
-summary = generate_question_trivia([tweet['text'] for tweet in tweet_objects])
-print(summary)
+    def summarize_list_of_tweets(self, tweets):
+        response = self.model.generate_content(f"""
+            Give me a one sentence summary of the following tweets from different authors: \n \n
+            {'\n'.join(tweets)}
+        """)
+        return response.text
+
+    def generate_question_trivia(self, tweets):
+        json_format = self.format._get_options_format()
+        response = self.model.generate_content(f"""
+            Create a one paragraph summary of each following tweets to generate trivia questions.
+            You must use the following self-explaining JSON format: \n\n {json_format}.  \n\n
+            {'\n'.join(tweets)}
+        """)
+        return response.text
+
+    def generate_question_guess_author(self, tweets):
+        json_format = self.format._get_options_format()
+        response = self.model.generate_content(f"""
+            Generate a question asking to guess the author of each following tweets.
+            You must use the following self-explaining JSON format: \n\n {json_format}.  \n\n
+            {'\n'.join(tweets)}
+        """)
+        return response.text
+
+    def generate_question_complete_tweet(self, tweets):
+        json_format = self.format._get_complete_format()
+        response = self.model.generate_content(f"""
+            For each of the following tweets, you must replace a word or a phrase with a blank space.
+            
+            You must use the following self-explaining JSON format: \n\n {json_format}.  \n\n
+            {'\n'.join(tweets)}
+        """)
+        return response.text
+
+    def generate_questions(self, question_type, tweets):
+        TYPES_FUNCTIONS = {
+            'trivia': self.generate_question_trivia,
+            'guess': self.generate_question_guess_author,
+            'complete': self.generate_question_complete_tweet,
+        }
+
+        question_func = TYPES_FUNCTIONS.get(question_type, lambda _: f"Invalid question type: {question_type}")
+
+        return question_func(tweets)
+    
+    def generate_question(self, question):
+        # TODO: remove me?
+        response = self.model.generate_content(f"""
+            Create a one paragraph summary of the following tweets from different authors to generate trivia questions.
+            Use the json format: {{"topic": "trivia", "question": "What is the question?", "options": ["option1", "option2", "option3", "option4"], "answer": "option1}}
+            Use the following tweets to generate the questions: \n \n
+            {'\n'.join(question)}
+        """)
+        return response.text
+
+
+if __name__ == "__main__":
+    json_file = 'data/tweets.json'
+    with open(json_file) as json_data:
+        tweet_objects = json.load(json_data)
+    tweets = [tweet['text'] for tweet in tweet_objects]
+
+    interface = GeminiInterface()
+    summary = interface.generate_questions('complete', tweets)
+    print(summary)
