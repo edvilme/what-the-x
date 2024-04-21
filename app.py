65837b38a22ff45d146f2944080a219e6bb64ec6
Rostro Edgar <rostroedgar@Edgars-MacBook-Air.local>
2024-04-21 07:19:19 -0700

    algo2

--- app.py
+++ app.py
@@ -104,6 +104,10 @@ def q(username, quiz):
 def question():
     return render_template("question.html", question=random.choice(QUESTIONS))
 
+@app.route("/index")
+def index():
+    return render_template("index.html", question=random.choice(QUESTIONS))
+
 @app.route("/answer/<int:question>", methods=["POST"])
 def answer(question):
     if not session.get("user_token"):
